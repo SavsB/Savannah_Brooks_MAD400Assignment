42097297b8a3bac1da3a2001a4ef2baf499b52ae
SBrooks1999 <w8000397@myscc.ca>
2023-02-01 17:19:49 -0500

    Step 10 Complete

--- S_Brooks_MyFavouriteBands/src/app/models/content-list.ts
+++ S_Brooks_MyFavouriteBands/src/app/models/content-list.ts
@@ -1,23 +1,4 @@
 import { IContent } from "./icontent";
 
 export class ContentList {
-    static contentCount = 0;
-
-    private _items: IContent[];
-    
-    constructor(item: IContent){
-        this._items = [];
-    }
-
-    get items(): IContent[]{
-        return this._items;
-    }
-}
-
-function getLength(obj: IContent[]){
-    return obj.length
-}
-
-function addContent(obj: IContent[]){
-    return  ++ContentList.contentCount
 }
